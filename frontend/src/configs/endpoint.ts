9f9cfe38d007a7e5a22368cbfd40dbcbbdc9c7c9
puddingcodes777 <lucas.coincodes0313@gmail.com>
2025-11-15 11:37:27 -0500

    change constants

--- frontend/src/configs/endpoint.ts
+++ frontend/src/configs/endpoint.ts
@@ -1,9 +1,9 @@
-// export const API_ENDPOINTS = {
-//   UPLOAD: `/board/upload`,
-//   GET_CURRENT: (boardId: number) => `/board/${boardId}/current`,
-//   GET_NEXT: (boardId: number) => `/board/${boardId}/next`,
-//   GET_N_NEXT: (boardId: number, generationNum: number) => `/board/${boardId}/nextN/${generationNum}`,
-//   GET_STABLE: `/board/simulate`,
-//   GETALL: `/board/all`,
-//   DELETE: (boardId: number) => `/board/${boardId}`
-// }
+export const API_ENDPOINTS = {
+  UPLOAD: `/board/upload`,
+  GET_CURRENT: (boardId: number) => `/board/${boardId}/current`,
+  GET_NEXT: (boardId: number) => `/board/${boardId}/next`,
+  GET_N_NEXT: (boardId: number, generationNum: number) => `/board/${boardId}/nextN/${generationNum}`,
+  GET_STABLE: `/board/simulate`,
+  GETALL: `/board/all`,
+  DELETE: (boardId: number) => `/board/${boardId}`
+}

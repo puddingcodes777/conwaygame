021c235fb348f071169d8e35467be60c0ab50866
christian richardson <christianrichardson.finance@gmail.com>
2025-12-05 23:21:46 -0500

    fix config

--- frontend/src/configs/endpoint.ts
+++ frontend/src/configs/endpoint.ts
@@ -3,7 +3,7 @@ export const API_ENDPOINTS = {
   GET_CURRENT: (boardId: number) => `/board/${boardId}/current`,
   GET_NEXT: (boardId: number) => `/board/${boardId}/next`,
   GET_N_NEXT: (boardId: number, generationNum: number) => `/board/${boardId}/nextN/${generationNum}`,
-  GET_STABLE: `/board/simulate`,
-  GETALL: `/board/all`,
+  // GET_STABLE: `/board/simulate`,
+  // GETALL: `/board/all`,
   DELETE: (boardId: number) => `/board/${boardId}`
 }
